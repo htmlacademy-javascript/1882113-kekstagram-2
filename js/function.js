4bae0e6c789fffb9ca6f4d8c123d06486278ce77
Elisanov Nikolay <Afterlaiv1@yandex.ru>
2024-12-18 18:22:12 +0300

    add js module

--- js/function.js
+++ js/function.js
@@ -29,24 +29,3 @@ function getNumberFromString(string) {
 }
 
 getNumberFromString('ECMAScript 2022');
-
-const checksDurationMeeting = (startWorks, endWorks, startMeeting, durationMeeting) => {
-  startWorks = startWorks.split(':');
-  endWorks = endWorks.split(':');
-  startMeeting = startMeeting.split(':');
-
-  const durationMeetingMin = durationMeeting % 60;
-  const durationMeetingHour = (durationMeeting - durationMeetingMin) / 60;
-  const startWorksNumber = +`${startWorks[0]}.${startWorks[1]}`;
-  const endtWorksNumber = +`${endWorks[0]}.${endWorks[1]}`;
-  const startMeetingNumber = +`${startMeeting[0]}.${startMeeting[1]}`;
-  const durationMeetingNumber = +`${durationMeetingHour}.${durationMeetingMin}`;
-  const endMeeting = startMeetingNumber + durationMeetingNumber;
-
-  if(startWorksNumber <= startMeetingNumber && endtWorksNumber >= endMeeting) {
-    return true;
-  }
-  return false;
-};
-
-checksDurationMeeting('08:00', '17:30', '14:00', 90);
