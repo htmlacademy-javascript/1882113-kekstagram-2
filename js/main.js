f80aa95bcd45c4908c4f05d6c44afcbfe3eed3b5
Elisanov Nikolay <Afterlaiv1@yandex.ru>
2024-12-22 16:05:06 +0300

    refactoring block

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
-import {createPhotoData} from './data.js';
-createPhotoData();
+import {
+  createPhotoData,
+  NUMBER_OF_OBJECTS_GENERATED
+} from './create-foto-data.js';
+createPhotoData(NUMBER_OF_OBJECTS_GENERATED);
