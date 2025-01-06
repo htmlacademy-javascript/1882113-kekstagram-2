1318d4fcd9efdaa11d7b3b5092ca5b6159172b79
Elisanov Nikolay <Afterlaiv1@yandex.ru>
2025-01-06 13:32:39 +0300

    refactoring miniature.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,2 @@
-import {
-  createPhotoData,
-  NUMBER_OF_OBJECTS_GENERATED
-} from './create-foto-data.js';
+import './create-foto-data.js';
 import './miniature.js';
-createPhotoData(NUMBER_OF_OBJECTS_GENERATED);
--- js/miniature.js
+++ js/miniature.js
@@ -3,17 +3,22 @@ import {
   NUMBER_OF_OBJECTS_GENERATED,
 } from './create-foto-data.js';
 const template = document.querySelector('#picture').content;
-const miniaturesData = createPhotoData(NUMBER_OF_OBJECTS_GENERATED);
+const thumbnailsData = createPhotoData(NUMBER_OF_OBJECTS_GENERATED);
 const fragment = document.createDocumentFragment();
 const pictures = document.querySelector('.pictures');
 
-miniaturesData.forEach(({description, likes, url, comments}) => {
+const createMiniatures = ({description, likes, url, comments}) => {
   const templateItem = template.cloneNode(true);
-  templateItem.querySelector('.picture__img').src = url;
-  templateItem.querySelector('.picture__img').alt = description;
+  const thumbnail = templateItem.querySelector('.picture__img');
+  thumbnail.src = url;
+  thumbnail.alt = description;
   templateItem.querySelector('.picture__likes').textContent = likes;
   templateItem.querySelector('.picture__comments').textContent = comments.length;
-  fragment.appendChild(templateItem);
+  return templateItem;
+};
+
+thumbnailsData.forEach((thumbnailData) => {
+  fragment.appendChild(createMiniatures(thumbnailData));
 });
 
 pictures.appendChild(fragment);
