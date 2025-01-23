b855d98e007fd94e347e3fed40c8c1fecf68f21e
Elisanov Nikolay <Afterlaiv1@yandex.ru>
2025-01-23 19:13:12 +0300

    added validate-form

---
+++ js/upload-modal.js
@@ -0,0 +1,40 @@
+const uploadFile = document.querySelector('.img-upload__input');
+const uploadOverlay = document.querySelector('.img-upload__overlay');
+const buttonClose = uploadOverlay.querySelector('.img-upload__cancel');
+const inputHashtags = uploadOverlay.querySelector('.text__hashtags');
+const inputDescription = uploadOverlay.querySelector('.text__description');
+//
+// uploadOverlay.classList.remove('hidden');
+//
+
+const buttonCloseHendler = () => {
+  uploadOverlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  uploadFile.value = '';
+  inputHashtags.value = '';
+  inputDescription.value = '';
+  inputHashtags.removeEventListener('keydown', removeCloseKeydown);
+  inputDescription.removeEventListener('keydown', removeCloseKeydown);
+  document.removeEventListener('keydown', keyCloseHendler);
+  buttonClose.removeEventListener('click', buttonCloseHendler);
+};
+
+function keyCloseHendler(evt) {
+  if(evt.key === 'Escape') {
+    buttonCloseHendler();
+  }
+}
+
+function removeCloseKeydown(evt) {
+  evt.stopPropagation();
+}
+
+
+uploadFile.addEventListener('change', () => {
+  uploadOverlay.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  buttonClose.addEventListener('click', buttonCloseHendler);
+  document.addEventListener('keydown', keyCloseHendler);
+  inputHashtags.addEventListener('keydown', removeCloseKeydown);
+  inputDescription.addEventListener('keydown', removeCloseKeydown);
+});
--- js/validate-form.js
+++ js/validate-form.js
@@ -1,22 +1,67 @@
-const uploadFile = document.querySelector('#upload-file');
-const uploadOverlay = document.querySelector('.img-upload__overlay');
-const buttonClose = uploadOverlay.querySelector('.img-upload__cancel');
+const uploadForm = document.querySelector('.img-upload__form');
+const inputHashtags = uploadForm.querySelector('.text__hashtags');
+const inputDescription = uploadForm.querySelector('.text__description');
+const patternHashtags = /^#[a-zа-яё0-9]{1,19}$/i;
+let string;
 
-//
-// uploadOverlay.classList.remove('hidden');
-//
+const pristine = new Pristine(uploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'div',
+  errorTextClass: 'img-upload__field-wrapper--error'
+}, true);
 
-const buttonCloseHendler = () => {
-  uploadOverlay.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-  buttonClose.removeEventListener('click', buttonCloseHendler);
-  uploadFile.value = '';
-  console.log(uploadFile.value);
+const errorText = () => `${string}`;
+
+const validateHashtags = (value)=> {
+  const validateArray = value.split(' ');
+  if(validateArray.length <= 5) {
+    for(let i = 0; i < validateArray.length; i++) {
+      for(let j = i + 1; j < validateArray.length; j++) {
+        if(validateArray[j] === validateArray[i]) {
+          string = 'хэштеги повторяются';
+          console.log('не верно');
+          return false;
+        }
+      }
+      if(validateArray[i] !== '') {
+        const isValid = patternHashtags.test(validateArray[i]);
+        if(!isValid) {
+          console.log('не верно');
+          string = 'введён невалидный хэштег';
+          return false;
+        }
+      }
+    }
+    console.log('валидно');
+    return true;
+  }
+  console.log('не верно');
+  string = 'превышено количество хэштегов';
+  return false;
+
+
+};
+
+const validateSringLength = (value) => {
+  if(value.length > 140) {
+    string = 'длина комментария больше 140 символов';
+    return false;
+  }
+  return true;
 };
 
 
-uploadFile.addEventListener('change', () => {
-  uploadOverlay.classList.remove('hidden');
-  document.body.classList.add('modal-open');
-  buttonClose.addEventListener('click', buttonCloseHendler);
+pristine.addValidator(inputHashtags, validateHashtags, errorText);
+pristine.addValidator(inputDescription, validateSringLength, errorText);
+
+
+uploadForm.addEventListener('submit', (evt) => {
+  const isValid = pristine.validate();
+  if(isValid) {
+    console.log('1');
+  } else {
+    evt.preventDefault();
+    console.log('2');
+  }
 });
